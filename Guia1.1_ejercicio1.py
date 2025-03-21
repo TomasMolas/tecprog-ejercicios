f7774995b50c3f981f439adc52f9fcb3927e74f6
Tomas Molas <tutomolas@gmail.com>
2025-03-20 21:07:36 -0300

    Guia1_ejer1

---
+++ Guia1.1_ejercicio1.py
@@ -0,0 +1,6 @@
+#Desarrolla un programa que pida al usuario su año de nacimiento y el año actual, y calcule su edad.
+
+nacimiento = int(input("Introduce tu año de nacimiento: "))
+actual = int(input("Introduce el año actual: "))
+
+print("tu edad es: ", actual-nacimiento)
